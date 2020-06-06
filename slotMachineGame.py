0aaba162e5349c3e0de53e2cbe10cd87fe1e0c7e
Shoaib Adnan <shoaibadnan870@gmail.com>
2020-06-06 11:47:23 -0230

    Added basic outline and prizes for the slot machine

---
+++ slotMachineGame.py
@@ -0,0 +1,24 @@
+'''Author: Shoaib Adnan
+Project name: slotMachineGame
+Due date: June 17th
+Instructor: Eric Stock'''
+
+
+
+import random
+
+
+print()
+print('''Welcome to the python Slot Machine!
+You will start with $50 and you will be asked if you want to play.
+You can answer with yes/no or you can also use y/n
+The chart for winnings for individual combinations are as follows: 
+BAR\tBAR\tBAR\t\tpays\t$250
+BELL\tBELL\tBELL/BAR\tpays\t$20
+PLUM\tPLUM\tPLUM/BAR\tpays\t$14
+ORANGE\tORANGE\tORANGE/BAR\tpays\t$10
+CHERRY\tCHERRY\tCHERRY\t\tpays\t$7
+CHERRY\tCHERRY\t  -\t\tpays\t$5
+CHERRY\t  -\t  -\t\tpays\t$2
+7\t  7\t  7\t\tpays\tTHE JACKPOT!
+''')
